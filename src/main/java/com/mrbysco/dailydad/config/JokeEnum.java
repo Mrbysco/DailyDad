aafe0aba860fd3d2508e984e9e598c98cc5ac75e
Mrbysco <Mrbysco@users.noreply.github.com>
2022-02-07 23:01:57 +0100

    Increase version [push]

--- src/main/java/com/mrbysco/dailydad/config/JokeEnum.java
+++ src/main/java/com/mrbysco/dailydad/config/JokeEnum.java
@@ -1,17 +1,7 @@
 package com.mrbysco.dailydad.config;
 
 public enum JokeEnum {
-	LOADING(false),
-	CHAT(true),
-	TTS(false);
-
-	private final boolean withName;
-
-	JokeEnum(boolean withName) {
-		this.withName = withName;
-	}
-
-	public boolean isWithName() {
-		return withName;
-	}
+	LOADING,
+	CHAT,
+	TTS
 }
